5a8d48e16c3c8a9c33ba9914b3c057c3d074ab69
fazla23 <fazlarabbi366iou@gmail.com>
2024-08-29 13:01:38 +0600

    removed unused import

--- src/test/java/com/example/demoCICD/DemoCicdApplicationTest.java
+++ src/test/java/com/example/demoCICD/DemoCicdApplicationTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.logging.Logger;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class DemoCicdApplicationTest {
 
     static Logger logger = Logger.getLogger(DemoCicdApplicationTest.class.getName());
